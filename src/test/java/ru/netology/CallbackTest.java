03bb8e26c39ccd9ab1aca679bd4a79f012fefda9
lokky55 <lokky55@mail.ru>
2022-08-07 17:22:00 +0300

    css selectors added

--- src/test/java/ru/netology/CallbackTest.java
+++ src/test/java/ru/netology/CallbackTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class CallbackTest {
     private WebDriver driver;
 
-    @BeforeAll
+    @BeforeAll  // запускается один раз перед всеми тестами
     // делаем метод в котором устанавливаем свойства для драйвера (chrome driver из директории driver/win
     static void setUpAll() {
         System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
@@ -32,8 +32,8 @@ public class CallbackTest {
     public void test() {
         driver.get("http://localhost:9999"); // открываем браузер
         // набираем костяк теста без описания функции findElement
-        driver.findElement(By.cssSelector("[type=\"text\"]")).sendKeys("Александр");     // находим первый элемент - поле ввода имени, и вводим в него имя Андрей (командой sendkeys)
-        driver.findElement(By.cssSelector("[type=\"tel\"]")).sendKeys("+79807133080"); // заполняем следующее поле заявки - телефон
+        driver.findElement(By.cssSelector("span[data-test-id='name'] input")).sendKeys("Александр");     // находим первый элемент - поле ввода имени, и вводим в него имя Андрей (командой sendkeys)
+        driver.findElement(By.cssSelector("span[data-test-id='phone'] input")).sendKeys("+79807133080"); // заполняем следующее поле заявки - телефон
 
         driver.findElement(By.className("checkbox__box")).click(); // следующее поле - чекбокс, командой click ставим в нем галочку
         driver.findElement(By.tagName("button")).click(); // следующее поле - кнопка ОТПРАВИТЬ - нажимаем ее той же командой click
